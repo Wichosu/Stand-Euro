0a87db5da2922fa7aa4ea8cd3b3f1552fb7f5ce3
Wicho <luiscalderonmiranda@gmail.com>
2020-02-02 15:47:53 -0600

    attemting to do the javaScript interactions

---
+++ javaScript/script.js
@@ -0,0 +1,10 @@
+const phonesButton = document.querySelector('.phones__button')
+
+
+phones.Button.addEventListener('click', unlockScreen)
+
+console.log(phonesButton)
+
+function unlockScreen (){
+    
+}
