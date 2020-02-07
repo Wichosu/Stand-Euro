9e85a5414e52a3cae8df0ac88350b5fe0a9ad89b
Wicho <luiscalderonmiranda@gmail.com>
2020-02-07 16:54:54 -0600

    working on Sony phones animation

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,14 +1,30 @@
+//
+// ─── IPHONE VARIABLES ───────────────────────────────────────────────────────────
+//
 const phonesPower = document.querySelector('.phones__power')
 const phonesScreen = document.querySelector('.phones__screen')
-
-console.log(phonesPower, phonesScreen)
+//
+// ─── SONY VARIABLES ─────────────────────────────────────────────────────────────
+//
+const phonesPowerSony = document.querySelector('.phones__power--sony')
+const phonesScreenSony = document.querySelector('.phones__screen--sony')
+    
+console.log(phonesPower, phonesScreen, phonesPowerSony, phonesScreenSony)
 
 phonesPower.addEventListener('click', unlock)
 
-while(phonesScreen.classList.contains('active')){
-    phonesScreen.addEventListener()
-}
+//
+// ─── VOLUME ANIMATIONS??? ───────────────────────────────────────────────────────
+//
 
+// while(phonesScreen.classList.contains('active')){
+//     phonesScreen.addEventListener()
+// }
+
+//
+// ─── FUNCTIONS FOR BOTH PHONES CLASSES ──────────────────────────────────────────
+//
+    
 function unlock(){
     if(phonesScreen.classList.contains('active')){
         phonesScreen.classList.remove('active')
