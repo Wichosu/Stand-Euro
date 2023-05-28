06c8d3a16d0d4ae9841333b8a12ad2d8429ac5cf
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-05-27 22:20:02 -0600

    style for StandCard component

--- app/components/StandCard.jsx
+++ app/components/StandCard.jsx
@@ -14,7 +14,8 @@ export default function StandCard({ img, route, children}){
         <img 
           src={img} 
           alt={img} 
-          className="w-full opacity-40 hover:opacity-30 transition-all" 
+          className="w-full opacity-40 hover:opacity-30 transition-all
+          object-cover" 
         />
         <span className="absolute top-1/2 left-0 right-0 mx-auto w-fit 
           text-3xl uppercase"
