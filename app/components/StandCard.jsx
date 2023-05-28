9e92dddec19506c7c41ca09475fb24200f2351bc
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-05-27 22:35:13 -0600

    Small font for span>

--- app/components/StandCard.jsx
+++ app/components/StandCard.jsx
@@ -18,7 +18,7 @@ export default function StandCard({ img, route, children}){
           object-cover" 
         />
         <span className="absolute top-1/2 left-0 right-0 mx-auto w-fit 
-          text-3xl uppercase"
+          text-2xl lg:text-3xl uppercase"
         >
           { children }
         </span>
